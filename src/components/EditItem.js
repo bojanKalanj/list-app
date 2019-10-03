b737f53af65262050d3510aae762786400938518
bojanKalanj <bojan.kalanj@gmail.com>
2019-10-03 16:14:18 +0200

    Edit product functionality added

--- src/components/EditItem.js
+++ src/components/EditItem.js
@@ -1,8 +1,90 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class EditItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: null
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    var id = this.props.match.params.id;
+    axios
+      .get(`http://localhost:3001/api/v1/items/${id}`)
+      .then(response => {
+        this.setState({
+          product: response.data
+        });
+      })
+      .catch(error => console.log(error));
+  }
+
+  handleChange(e) {
+    var { name, value } = e.target;
+    var { product } = this.state;
+    product[name] = value;
+    this.setState({ product });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    var { product } = this.state;
+    axios
+      .patch(`http://localhost:3001/api/v1/items/${product.id}`, {
+        item: { ...product }
+      })
+      .then(response => {
+        console.log(response);
+        this.props.history.push("/");
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   render() {
-    return <div>EditItem</div>;
+    if (this.state.product) {
+      var { title, desc, price } = this.state.product;
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input
+              name="title"
+              value={title}
+              onChange={this.handleChange}
+              type="text"
+            />
+          </label>
+          <label>
+            Description:
+            <textarea
+              name="desc"
+              value={desc}
+              onChange={this.handleChange}
+              type="text"
+            />
+          </label>
+          <label>
+            Price:
+            <input
+              name="price"
+              value={price}
+              onChange={this.handleChange}
+              type="text"
+            />
+          </label>
+          <button>Submit</button>
+        </form>
+      );
+    } else {
+      return <p>Loading...</p>;
+    }
   }
 }
 
--- src/components/ProductsListItem.js
+++ src/components/ProductsListItem.js
@@ -3,19 +3,20 @@ import { Link } from "react-router-dom";
 
 class ProductsListItem extends Component {
   render() {
-    var { product } = this.props;
+    var { product, deleteProdHandler } = this.props;
     return (
       <Fragment>
         <ul>
           <li>
-            <Link to={`/item/${product.id}`}>Name - {product.title}</Link>
+            <Link to={`/items/${product.id}`}>Name - {product.title}</Link>
           </li>
           <li>Description - {product.desc}</li>
           <li>Price - {product.price}</li>
         </ul>
-        <button onClick={() => this.props.deleteProdHandler(product.id)}>
+        <button onClick={deleteProdHandler.bind(this, product.id)}>
           Delete product
         </button>
+        <Link to={`/items/edit/${product.id}`}>Edit product</Link>
         <hr />
       </Fragment>
     );
