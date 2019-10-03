99d26b62d307e4dcdfd31418fcaad91264651af6
bojanKalanj <bojan.kalanj@gmail.com>
2019-10-03 12:58:35 +0200

    Delete product from database

--- src/containers/ProductsContainer.js
+++ src/containers/ProductsContainer.js
@@ -12,77 +12,6 @@ class ProductsContainer extends Component {
     };
   }
 
-  // componentWillMount() {
-  //   let products = [
-  //     {
-  //       id: 1,
-  //       name: "prod one",
-  //       desc: "egestas sed sed risus pretium",
-  //       price: 53423
-  //     },
-  //     {
-  //       id: 2,
-  //       name: "prod two",
-  //       desc: "egestas sed sed risus pretium",
-  //       price: 33
-  //     },
-  //     {
-  //       id: 3,
-  //       name: "prod three",
-  //       desc:
-  //         "egestas sed segestas sed sed risus pretiumegestas sed sed risus pretiumegestas sed sed risus pretiumed risus pretium",
-  //       price: 5
-  //     },
-  //     {
-  //       id: 4,
-  //       name: "prod four",
-  //       desc: "egestas sedegestas sed sed risus pretium sed risus pretium",
-  //       price: 4
-  //     },
-  //     {
-  //       id: 5,
-  //       name: "prod five",
-  //       desc:
-  //         "egestas sed sed risus pretiumegestas sed sed risus pretiumegestas sed sed risus pretium",
-  //       price: 54
-  //     },
-  //     {
-  //       id: 6,
-  //       name: "prod six",
-  //       desc: "egestas egestas sed sed risus pretiumsed sed risus pretium",
-  //       price: 55
-  //     },
-  //     {
-  //       id: 7,
-  //       name: "prod seven",
-  //       desc:
-  //         "egestas segestas sed sed risus pretiumegestas sed sed risus pretiumed sed risus pretium",
-  //       price: 5
-  //     },
-  //     {
-  //       id: 8,
-  //       name: "prod eight",
-  //       desc: "egeegestas sed sed risus pretiumvstas sed sed risus pretium",
-  //       price: 333
-  //     },
-  //     {
-  //       id: 9,
-  //       name: "prod nine",
-  //       desc: "egestas sed sed risuegestas sed sed risus pretiums pretium",
-  //       price: 11
-  //     },
-  //     {
-  //       id: 10,
-  //       name: "prod ten",
-  //       desc: "egestas sed sed risus pretium",
-  //       price: 88
-  //     }
-  //   ];
-  //
-  //   this.setState({
-  //     products: products
-  //   });
-  // }
   componentDidMount() {
     axios
       .get("http://localhost:3001/api/v1/items.json")
@@ -95,18 +24,14 @@ class ProductsContainer extends Component {
       .catch(error => console.log(error));
   }
 
-  deleteProdHandler = prodId => {
-    let products = this.state.products.filter(p => p.id !== prodId);
-
-    this.setState({
-      products: products
-    });
-
-    // this.setState({
-    //   products: this.state.products.filter(p => p.id !== prodId)
-    // });
-
-    console.log(this.state.products);
+  deleteProdHandler = id => {
+    axios
+      .delete(`http://localhost:3001/api/v1/items/${id}`)
+      .then(response => {
+        let products = this.state.products.filter(p => p.id !== id);
+        this.setState({ products });
+      })
+      .catch(error => console.log(error));
   };
 
   render() {
